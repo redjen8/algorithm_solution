64f1b4948d76adf418d364ed1657905870e3cf35
redjen8 <seokwoo2525@gmail.com>
2021-08-25 22:00:19 +0900

    boj 22937 solved

---
+++ python/boj_22937_20210825.py
@@ -0,0 +1,7 @@
+X1, Y1, R1 = map(int, input().split())
+X2, Y2, R2 = map(int, input().split())
+dist = (X1 - X2) ** 2 + (Y1 - Y2) ** 2
+if (R1 + R2) ** 2 > dist:
+    print('YES')
+else:
+    print('NO')
